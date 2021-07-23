02627f0e8c7624642438c0bccd79098c6f224b67
BrianH <brianhwang29@gmail.com>
2021-07-23 15:28:56 +0900

    basic setup

---
+++ todo/src/components/TodoList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <ul>
+      <li>hi</li>
+      <li>goodbye</li>
+      <li>what</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  ul{
+    list-style: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+  }
+  li{
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height:50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+  }
+  .removeBtn{
+    margin-left: auto;
+    color: #de4343;
+  }
+  .checkBtn{
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+  }
+  .checkBtnCompleted{
+    color:#b3adad;
+  }
+  .textCompleted{
+    text-decoration: line-through;
+    color:#b3adad;
+  }
+
+</style>
---
+++ todo/src/components/insertTodo.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text">
+    <span class="addContainer">
+      <i class="far fa-calendar-plus addBtn"></i>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+input:focus{
+  outline:none;
+}
+.inputBox{
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input{
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer{
+  float: right;
+  background: linear-gradient(to right, #18b940, #4fe23b);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+  size: 2rem;
+}
+.closeModalBtn {
+  color: #42b983;
+}
+</style>
